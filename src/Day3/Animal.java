4fcb083a13b4b8e365461143a634b2d531e42453
Your Name <chaitanyakodali440@gmail.com>
2026-08-19 13:05:31 +0530

    INITIAL COMMIT

---
+++ src/Day3/Animal.java
@@ -0,0 +1,19 @@
+package Day3;
+
+public class Animal {
+    void eat()
+    {
+        System.out.println("Dog eats ");
+    }
+    static class Dog extends Animal
+    {
+        void bark()
+        {
+            System.out.println("Dog Barks ");
+        }
+    }
+
+    public static void main(String[] args){
+        Dog d=new Dog(); d.eat(); d.bark();
+    }
+}
